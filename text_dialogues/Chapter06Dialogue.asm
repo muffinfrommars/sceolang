03243b1d07ce67d4c5727c69c5cfed85d8f43ee5
muffinfrommars <awesomedude307@gmail.com>
2025-01-16 09:01:33 -0800

    paralogue b

--- text_dialogues/Chapter06Dialogue.asm
+++ text_dialogues/Chapter06Dialogue.asm
@@ -54,3 +54,187 @@ paralogueVeld
 
     .byte EndText
 
+paralogueManfroy
+    .byte Right_slot
+    .word LoadPortrait
+    .word manfroy_portrait
+    .text "Hmm, so Raydrik has made it to Leonster, has he?"
+    .byte WaitForA
+
+    .byte Left_slot
+    .word LoadPortrait
+    .word Veld_Portrait
+    .text "Reportedly so, Your Eminence..."
+    .byte WaitForA
+
+    .byte Right_Slot
+    .word LoadPortrait
+    .word Travant_portrait
+    .byte ScrollText
+    .text "Remind me why I came all this way in"
+    .byte NewLine
+    .text "the middle the night?"
+    .byte WaitForA
+
+    .byte Left_Slot
+    .byte ScrollText
+    .word LoadPortrait
+    .word manfroy_portrait
+    .text "King Travant..."
+    .byte NewLine
+    .text "The battlefield is ever-changing."
+    .byte WaitForA
+
+    .byte ScrollText
+    .text "Kalf is no longer your primary target..."
+    .byte NewLine
+    .text "A new problem has arisen: Prince Quan."
+    .byte WaitForA
+
+    .byte ScrollText
+    .text "With him being wed to the Chalphy princess,"
+    .byte NewLine
+    .text "it is only a matter of time before an heir to"
+    .byte NewLine
+    .text "Gae Bolg is sired..."
+    .byte WaitForA
+
+    .byte Right_Slot
+    .word LoadPortrait
+    .word Veld_Portrait
+    .byte ScrollText
+    .text "Perhaps an heir who can wield both Gae Bolg"
+    .byte NewLine
+    .text "AND Tyrfing. A quarrel we do not wish to"
+    .byte NewLine
+    .text "deal with, surely?"
+    .byte WaitForA
+
+    .byte Left_Slot
+    .word LoadPortrait
+    .word Travant_Portrait
+    .byte ScrollText
+    .text "..."
+    .byte NewLine
+    .text "So, when does this happen?"
+    .byte WaitForA
+
+    .byte Right_Slot
+    .word LoadPortrait
+    .word manfroy_portrait
+    .byte ScrollText
+    .text "Good fortune come to those who wait,"
+    .byte NewLine
+    .text "Your Majesty..."
+    .byte NewLine
+    .text "This is not something to be done hastely."
+    .byte WaitForA
+
+    .byte ScrollText
+    .text "All in due time..."
+    .byte WaitForA
+
+    .byte Left_Slot
+    .byte ScrollText
+    .text "..."
+    .byte NewLine
+    .text "What about that blasted Raydrik?"
+    .byte NewLine
+    .text "Is he truly necessary?"
+    .byte WaitForA
+
+    .byte Right_Slot
+    .byte ScrollText
+    .text "That brute is only a small part of Bishop Veld's"
+    .byte NewLine
+    .text "brilliant plan to hand Thracia to its rightful ruler."
+    .byte WaitForA
+
+    .byte Left_Slot
+    .byte ScrollText
+    .text "I'm listening."
+    .byte WaitForA
+
+    .byte Right_Slot
+    .word LoadPortrait
+    .word Veld_Portrait
+    .byte ScrollText
+    .text "Upon returning from a night of celebrations,"
+    .byte NewLine
+    .text "King Auerbach will meet his end. Perhaps, one drink"
+    .byte NewLine
+    .text "too many, for such a fragile body..."
+    .byte WaitForA
+
+    .byte ScrollText
+    .text "It is only natural that Prince Carl, the King's"
+    .byte NewLine
+    .text "child brother, will succeed him."
+    .byte WaitForA
+
+    .byte ScrollText
+    .text "But, a boy so young is cannot be ready to rule a kingdom..."
+    .byte WaitForA
+
+    .byte ScrollText
+    .text "The boy will seek for guidance, naturally."
+    .byte NewLine
+    .text "And who better to listen to than his personal"
+    .byte NewLine
+    .text "guard, Sir Raydrik?"
+    .byte WaitForA
+
+    .byte Left_Slot
+    .byte ScrollText
+    .text "Surely even a child such as Carl can see Raydrik"
+    .byte NewLine
+    .text "for what he truly is: little more than "
+    .byte NewLine
+    .text "a schoolyard bully."
+    .byte WaitForA
+
+    .byte Right_Slot
+    .word LoadPortrait
+    .word manfroy_portrait
+    .byte ScrollText
+    .text "The child won't be needed for long..."
+    .byte NewLine
+    .text "And with no heir to inherit Connacht's throne,"
+    .byte NewLine
+    .text "perhaps, military rule becomes necessary?"
+    .byte WaitForA
+
+    .byte ScrollText
+    .text "Raydrik will create chaos upon an unclaimed crown."
+    .byte WaitForA
+
+    .byte Left_Slot
+    .byte ScrollText
+    .text "...you mean to tell me you plan on giving that"
+    .byte NewLine
+    .text "ape the power to rule a kingdom?"
+    .byte WaitForA
+
+    .byte ScrollText
+    .text "I still fail to see how this benefits my people."
+    .byte WaitForA
+
+    .byte Right_Slot
+    .byte ScrollText
+    .text "Need we spell it out for you, Your Majesty?"
+    .byte NewLine
+    .text "If it weren't for Gae Bolg and the Lanzenritter,"
+    .byte NewLine
+    .text "Connacht, not Leonster, would be strongest of all."
+    .byte WaitForA
+
+    .byte ScrollText
+    .text "The next time The District needs to band together,"
+    .byte NewLine
+    .text "half their might will be led by a raging lunatic."
+    .byte WaitForA
+
+
+
+    .byte EndText
+
