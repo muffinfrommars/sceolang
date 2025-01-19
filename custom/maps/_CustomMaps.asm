bb8de724475f9e9fc0a3a9ad8faa6d18c0ba93e1
muffinfrommars <awesomedude307@gmail.com>
2025-01-18 18:48:02 -0800

    push!

--- custom/maps/_CustomMaps.asm
+++ custom/maps/_CustomMaps.asm
@@ -26,7 +26,8 @@
     leonstercastle   .binary "leonstercastle.bin.fe4"
     eastthracia      .binary "eastthracia.bin.fe4"
     debug2           .binary "debug2.bin.fe4"
-    ballroom1         .binary "ballroom.bin.fe4"
+    ballroom1        .binary "ballroom.bin.fe4"
+    dream            .binary "dream.bin.fe4"
     ;nightroad        .binary "nightroad.bin.fe4"
     ;mapleif          .binary "leif.bin.fe4"
 .here
